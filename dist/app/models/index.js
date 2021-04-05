dda70321e9b31a037a159137258a050342857a61
dwri050 <dwri050@aucklanduni.ac.nz>
2021-04-06 04:45:57 +1200

    correting database file path

--- dist/app/models/index.js
+++ dist/app/models/index.js
@@ -1,12 +1,15 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");     //Squelize (caps) is a class.
-
+var pathResolver = require('path')
 //squelize (lowercase) is an instance of a Squelize object.
 //Build it and configure it to use our backend file.
+
+var dbPath = pathResolver.join(__dirname,"../persistence/marcdatabase.sqlite")
+console.log("sqlite database path is" + dbPath);
 const sequelize = new Sequelize({       
   dialect: "sqlite",
-  storage: "./dist/app/database/marcdatabase.sqlite"
+  storage: dbPath
 });
 
 const db = {};
