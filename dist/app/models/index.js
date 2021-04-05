a97739d6b69d1b00c05ee6f7cdc4d204e464de17
dwri050 <dwri050@aucklanduni.ac.nz>
2021-04-05 22:06:32 +1200

    added helpful remarks to database constructor to explain syntax

--- dist/app/models/index.js
+++ dist/app/models/index.js
@@ -1,17 +1,18 @@
 const dbConfig = require("../config/db.config.js");
 
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize");     //Squelize (caps) is a class.
 
-const sequelize = new Sequelize({
+//squelize (lowercase) is an instance of a Squelize object.
+//Build it and configure it to use our backend file.
+const sequelize = new Sequelize({       
   dialect: "sqlite",
   storage: "./dist/app/database/marcdatabase.sqlite"
 });
 
 const db = {};
-
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize); 
 
 module.exports = db;
