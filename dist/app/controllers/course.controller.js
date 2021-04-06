be4f032abe9291c65bd98db16d2d55b881a6432e
Steve-XD <qkan100@aucklanduni.ac.nz>
2021-04-06 21:24:33 +1200

    Change variable Tutorial to Course pt2

--- dist/app/controllers/course.controller.js
+++ dist/app/controllers/course.controller.js
@@ -1,38 +1,38 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
+const Course = db.courses;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
+// Create and Save a new Course
 exports.create = (req, res) => {
   
 };
 
-// Retrieve all Tutorials from the database.
+// Retrieve all Courses from the database.
 exports.findAll = (req, res) => {
   
 };
 
-// Find a single Tutorial with an id
+// Find a single Course with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a Tutorial by the id in the request
+// Update a Course by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Course with the specified id in the request
 exports.delete = (req, res) => {
   
 };
 
-// Delete all Tutorials from the database.
+// Delete all Courses from the database.
 exports.deleteAll = (req, res) => {
   
 };
 
-// Find all published Tutorials
+// Find all published Courses
 exports.findAllPublished = (req, res) => {
   
 };
