10c844ca5226109a2831dadcdc2956c0adc3f64c
dwri050 <dwri050@aucklanduni.ac.nz>
2021-04-11 14:38:11 +1200

    structure refactorization complete. boots. no database yet

---
+++ src/test/backend/Server_testing.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+const controller = require("../../controllers/course.controller");
+var router = require("express").Router();
+var request = require('supertest');
+
+// https://glebbahmutov.com/blog/how-to-correctly-unit-test-express-server/
+describe('Testing express server', function () {
+  var server;
+  beforeEach(function () {
+    delete require.cache[require.resolve('../../server.js')];
+    server = require('../../server.js');
+  });
+  afterEach(function (done) { // close the server when test finishes 
+    server.close(done);
+  });
+  it('responds to /', function testSlash(done) {
+  request(server)
+    .get('/')
+    .expect(200, done);
+  });
+  it('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/blablabla')
+      .expect(404, done);
+  });
+});
+
+
