1a2ad242396cbed1c5adc42e17d3081adcb833b6
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2021-04-14 13:26:38 +1200

    Marker db controller and company prelim

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 
-global.db.users=require("./User.js");
-global.db.courses=require("./Course.js")(global.db);  //IMPORTANT: ALWAYS supply an argument here, even tho global.db is global. If you dont supply an argument, Sequelize WILL NOT CREATE THE OBJECT. This took me 7 HOURS OF TORTURE to figure out. please, dont suffer the same fate as i. i have aged and my kids have grown up. i dont recognize the world anymore. please, dont omit an argument in this.
+global.db.users = require("./User.js");
+global.db.courses = require("./Course.js")(global.db);  //IMPORTANT: ALWAYS supply an argument here, even tho global.db is global. If you dont supply an argument, Sequelize WILL NOT CREATE THE OBJECT. This took me 7 HOURS OF TORTURE to figure out. please, dont suffer the same fate as i. i have aged and my kids have grown up. i dont recognize the world anymore. please, dont omit an argument in this.
+global.db.markers = require("./markers.js")(global.db);
 module.exports=db;        //Controllers look for db, then classes off that.
 
--- src/routes/marker.routes.js
+++ src/routes/marker.routes.js
@@ -1,8 +1,10 @@
-const {Router} = require("express");
-const marker = require("../controllers/marker.controller"); // get marker controller file
-const router = Router();
+module.exports = (app) => {
+    const {Router} = require("express");
+    const marker = require("../controllers/marker.controller"); // get marker controller file
+    const router = Router();
+}
+    
 
 //Make routes here to add delete etc
 
 
-export default router;
