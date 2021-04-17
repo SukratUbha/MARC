170e03ba14668366c9432117bfae7cb9a655ef32
dwri050 <dwri050@aucklanduni.ac.nz>
2021-04-18 03:26:06 +1200

    creates dummy student entries

--- src/controllers/dummydata.controller.js
+++ src/controllers/dummydata.controller.js
@@ -1,17 +1,18 @@
 // Hello. I create example data.
 
+const { Promise } = require('sequelize');
+
+
+
 //make database if its not already alive
 require(__dirname+'/database_creator.js');      //creats global.db
 require(__dirname+"/../Models/index.js") // Create models (& thus tables)
 
-function makeStudent(){
-    const Student= require(__dirname+'/../models/Student.js');//fetch the Model definition
+const Student= require(__dirname+'/../models/Student.js');//fetch the Model definition
 
-    const jane = Student.create({ firstName: randomName(), lastName: randomName() });
-    console.log("Created Student ", jane.firstName + " " + jane.lastName);
-   
+global.db.sync();                     // Persist to database first time. Call this often.
 
-}
+
 function aFew(){
     return 4+Math.random()*3;
 }
@@ -33,8 +34,24 @@ function randomChar(str){
     return str.charAt(Math.floor(Math.random()*str.length ));
 }
 
+function destroyStudents(){
+    global.db.students.drop();
+    
+}
+async function makeStudent(){
+
+    
+    jane = await Student.create({ firstName: randomName(), lastName: randomName() });
+    global.db.sync();                     // Persist to database first time. Call this often.
+
+    //console.log("Created Student "+ jane.firstName + " " + jane.lastName);
+    console.log("made:"+jane.firstName);
+
+}
+
 for (var i=0;i<aFew();i++){
     makeStudent();        
 }
-
+global.db.sync();
+//destroyStudents();
 console.log(randomName());
