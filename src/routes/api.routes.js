b6ce808b7efe38c1906f8eeecc1672dca94a485b
dwri050 <dwri050@aucklanduni.ac.nz>
2021-04-18 00:12:45 +1200

    added URL to documen that describes what API stuff we're making

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,3 +1,11 @@
+// THE API!!
+// here is a document that explains what API endpoints are going to make
+//
+//  https://docs.google.com/document/d/1zRuxYvO3SYmXhSJvM2NTjmoMs93iyczdGUOvYj1StrI/edit?usp=sharing
+// 
+// note that the API is all about stuff the client wants. Whatever tables we use behind the scenes can be anything.
+//
+
 module.exports =(app) => {
 
     const association = require("../controllers/api.controller.js"); //instantiating a new object
