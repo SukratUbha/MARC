a3aedf1cd94ea4c62f1f5a8e98fad454d677a7fa
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2021-04-21 13:01:59 +1200

    A simple file upload for submission html

--- src/routes/frontend.routes.js
+++ src/routes/frontend.routes.js
@@ -1,6 +1,7 @@
 module.exports = (app)=>{
     express=require("express");
     path=require("path");
+    const upload = require('express-fileupload');
     frontEndDir = path.join(__dirname, '../frontend');
     app.use('/', express.static(frontEndDir))    //browser finds /index.html in here i think. 
 
@@ -10,6 +11,7 @@ module.exports = (app)=>{
     //res.text("hey boss, its spencer testing git push");
   });
   //Example: Dish up a static HTML file at an endpoint (with exception checking)
+  app.use(upload());
   app.get("/submit", (req,res)=>{
 
     var options = {
@@ -21,14 +23,29 @@ module.exports = (app)=>{
         }
       }
     
-    res.sendFile(
+    res.sendFile(  
       "submission.html",                                //res.sendFile needs a file to dish up on this url
       options,
       (err)=>{ if (err) console.log("an error occured when sending file:"+err)}   //res.sendFile needs to be provided with an anonymous function to execute if theres an error
       );
     }
   );
+  app.post("/submit", (req,res) => {
+     if(req.files) { 
+      //  console.log(req.files);
+       var file = req.files.studentCV;
+       var filename = file.name;
+       console.log(file.name);
 
+       file.mv(path.join(__dirname,'../Uploads/') + filename, (err) =>{
+         if(err){
+           res.send(err);
+         } else {
+           res.send("File Uploaded")
+         }
+       });
+     }
+  })
 
 
 }
