3ac0f45fc966d585f20c14b0551bf47ac9be74e5
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2021-05-07 16:03:38 +1200

    Correction

--- Backend_NodeJS/routes/mailer.routes.js
+++ Backend_NodeJS/routes/mailer.routes.js
@@ -6,50 +6,50 @@ module.exports =(app) => {
     // var cors = require('cors');
     // const creds = require('./config');
 
-    var transport = {
-        host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
-        port: 587,
-        auth: {
-        user: creds.USER,
-        pass: creds.PASS
-    }
-    }
+    // var transport = {
+    //     host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
+    //     port: 587,
+    //     auth: {
+    //     user: creds.USER,   //Can include this info in the config file
+    //     pass: creds.PASS
+    // }
+    // }
 
-    var transporter = nodemailer.createTransport(transport)
+    // var transporter = nodemailer.createTransport(transport)
 
-    transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take messages');
-    }
-    });
+    // transporter.verify((error, success) => {
+    // if (error) {
+    //     console.log(error);
+    // } else {
+    //     console.log('Server is ready to take messages');
+    // }
+    // });
 
-    router.post("/api/sendMyEmail", (req, res, next) => {
-    var subject = req.body.subject
-    var email = req.body.email
-    var message = req.body.message
-    var content = `subject: ${subject} \n email: ${email} \n message: ${message} `
+    // router.post("/api/sendMyEmail", (req, res, next) => {
+    // var subject = req.body.subject
+    // var email = req.body.email
+    // var message = req.body.message
+    // var content = `subject: ${subject} \n email: ${email} \n message: ${message} `
 
-    var mail = {
-        from: email,
-        to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
-        subject: subject,
-        text: content
-    }
+    // var mail = {
+    //     from: email,
+    //     to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
+    //     subject: subject,
+    //     text: content
+    // }
 
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {
-        res.json({
-            status: 'fail'
-        })
-        } else {
-        res.json({
-        status: 'success'
-        })
-        }
-    })
-    })
+    // transporter.sendMail(mail, (err, data) => {
+    //     if (err) {
+    //     res.json({
+    //         status: 'fail'
+    //     })
+    //     } else {
+    //     res.json({
+    //     status: 'success'
+    //     })
+    //     }
+    // })
+    // })
 
     // app.post("/api/sendMyEmail", (req,res)=>{
     //     console.log("IAMHERE22");
