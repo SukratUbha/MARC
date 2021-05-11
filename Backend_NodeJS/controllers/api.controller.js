025e0aedc01a3db33069bca0c38d33008063f872
Steve-XD <qkan100@aucklanduni.ac.nz>
2021-05-11 20:14:57 +1200

    Update api.controller.js

--- Backend_NodeJS/controllers/api.controller.js
+++ Backend_NodeJS/controllers/api.controller.js
@@ -1,6 +1,10 @@
 // A variety of functions which create/modify Courses in the database.
 // The functions take in req/res (request/response) because they respond to
 // API endpoints (routes).
+
+
+
+
 //the Fields mentioned here must match the fields created in /models/Course.model.js
 
 // TODO: sync() the database when needed.
@@ -16,12 +20,6 @@ const Op = require("sequelize").Op;
 
 
 exports.createCourse = (req, res) => {
-  
-  //naive way to create course, will cause a lot of error showing on the terminal
-  //const Cs101 = Course.create({ id:1, Course_name:"CS101", CC:"Damir", CC_email:"damir@gmail.com", Total_student:null, comment:null});
-  //const Cs120 = Course.create({ id:2, Course_name:"CS120", CC:"Tanya", CC_email:"tanya@gmail.com", Total_student:230, comment:null});
-  //const Cs130 = Course.create({ id:3, Course_name:"CS130", CC:"Bukhard", CC_email:"bukhard@gmail.com", Total_student:480, comment:null});
-  //res.send("3 courses created")
 
   // Validate request
   if (!(req.body.Course_name&&req.body.CC&&req.body.CC_email)) {
@@ -34,11 +32,15 @@ exports.createCourse = (req, res) => {
   
   // Create a Course
   const course = {
-    Course_name: req.body.Course_name,
-    CC: req.body.CC,
-    CC_email: req.body.CC_email,
-    Total_student: null,
-    comment: null
+    Course_name:req.body.Course_name, 
+    CC:req.body.CC, 
+    CC_email:req.body.CC_email, 
+    Year: req.body.Year, 
+    Deadline: null,
+    Hours: null,
+    Total_student: null, 
+    comment_CC:null,
+    comment_MC:null
   };
   console.log(course);
 
@@ -70,7 +72,7 @@ exports.getAllCourses = (req, res) => {
       });
     });
 };
-
+/*
 exports.getCourse = (req, res) => {
   const id = req.params.id;
 
@@ -83,7 +85,7 @@ exports.getCourse = (req, res) => {
         message: "Error retrieving course with id=" + id
       });
     });
-};
+};*/
 
 // Update a Course by the id in the request
 exports.update_Total_Student = (req, res) => {
@@ -109,6 +111,70 @@ exports.update_Total_Student = (req, res) => {
         message: "Error updating Course with id=" + id
       });
     });
+};
+
+exports.loadvalue = (req, res) => {
+  //naive way to create course, will cause a lot of error showing on the terminal
+  Course.create({ Course_name:"CS373", 
+                  CC:"Bukhard", 
+                  CC_email:"bukhard@gmail.com", 
+                  Deadline: '2021-10-28', 
+                  Year: 3, 
+                  Hours: 2,
+                  Total_student:123, 
+                  comment_CC:null,
+                  comment_MC:null});
+
+  Course.create({ Course_name:"CS101", 
+                  CC:"Damir", 
+                  CC_email:"damir@gmail.com", 
+                  Deadline: '2021-08-09', 
+                  Year: 1, 
+                  Hours: 1.4,
+                  Total_student:500, 
+                  comment_CC:null,
+                  comment_MC:null});
+
+  Course.create({ Course_name:"CS120", 
+                  CC:"Tanya", 
+                  CC_email:"tanya@gmail.com", 
+                  Deadline: '2021-09-09', 
+                  Year: 1, 
+                  Hours: 1.6,
+                  Total_student:300, 
+                  comment_CC:null,
+                  comment_MC:null});
+
+  Course.create({ Course_name:"CS316", 
+                  CC:"Rizwan", 
+                  CC_email:"rizwan@gmail.com", 
+                  Deadline: '2021-9-20', 
+                  Year: 3, 
+                  Hours: 2,
+                  Total_student:150, 
+                  comment_CC:null,
+                  comment_MC:null});
+
+  Course.create({ Course_name:"CS210", 
+                  CC:"Bruce", 
+                  CC_email:"bruce@gmail.com", 
+                  Deadline: '2021-8-20', 
+                  Year: 2, 
+                  Hours: 2.5,
+                  Total_student:300, 
+                  comment_CC:null,
+                  comment_MC:null});
+
+  Course.create({ Course_name:"CS340", 
+                  CC:"Robert", 
+                  CC_email:"robert@gmail.com", 
+                  Deadline: '2021-8-20', 
+                  Year: 3, 
+                  Hours: 2.6,
+                  Total_student:113, 
+                  comment_CC:null,
+                  comment_MC:null});
+  res.send("Courses created")
 };
 /*
 // Retrieve all Courses from the database.
@@ -223,8 +289,15 @@ exports.findAllPublished = (req, res) => {
       });
     });
 };
+
+exports.createCourse = (req, res) => {
+  //naive way to create course, will cause a lot of error showing on the terminal
+  const Cs101 = Course.create({ id:1, Course_name:"CS101", CC:"Damir", CC_email:"damir@gmail.com", Total_student:null, comment:null});
+  const Cs120 = Course.create({ id:2, Course_name:"CS120", CC:"Tanya", CC_email:"tanya@gmail.com", Total_student:230, comment:null});
+  const Cs130 = Course.create({ id:3, Course_name:"CS130", CC:"Bukhard", CC_email:"bukhard@gmail.com", Total_student:480, comment:null});
+  res.send("3 courses created")
+};
 */
-// Create a student.
 exports.createStudent = (req, res) => {
   // Create a student
   const student = {
@@ -284,7 +357,6 @@ exports.createAss = (req, res) => {
     marking: false,
     time: Null
   };
-
   // Save Course in the database
   Association.create(ass)
     .then(data => {
