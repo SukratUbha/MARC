25be723dc4e9be34bb4de93fc49d505e295d1aa4
SukratUbha <54665232+SukratUbha@users.noreply.github.com>
2021-05-18 11:21:12 +1200

    Functional Register form

--- Backend_NodeJS/routes/students.routes.js
+++ Backend_NodeJS/routes/students.routes.js
@@ -9,18 +9,22 @@ module.exports =(app) => {
     const pipeline = promisify(require("stream").pipeline);
 
     //used to store registration form file and data
-    router.post("/api/uploadregistrationform", upload.single("file"), async function(req, res, next) {  //upload form needs to be connected
+    app.post("/api/uploadregistrationform", upload.single("file"), async function(req, res, next) {  //upload form needs to be connected
         const {
           file,
-          body: { name,
-                  //lastName,
-                  //email,
-                  //password,
-                  //and the rest
+          body: { firstName,
+                  lastName,
+                  email,
+                  fPref,
+                  hours,
+                  description
                  }
         } = req;
-      
-        const fileName = firstname + lastName + file.detectedFileExtension;
+        console.log(req.body.hours);
+        console.log(`${__dirname}../Uploads/`);
+        
+        const fileName = firstName + lastName + file.detectedFileExtension;
+        console.log(fileName);
         await pipeline(
           file.stream,
           fs.createWriteStream(`${__dirname}/../Uploads/${fileName}`)
@@ -32,20 +36,6 @@ module.exports =(app) => {
     // Create a new Student
     router.post("/api/registerStudent", register.registerStudent)   //Saw the add to server request on register.js, this can be modified to crud to db. 
     //So, ill leave this here until further clarity under /api/registerStudent  
-
-    app.post("/api/markerstest", (req,res)=>{
-      const {
-        file,
-        body: { name,
-                //lastName,
-                //email,
-                //password,
-                //and the rest
-               }
-      } = req;
-      console.log(req.body);
-      //console.log(req.body.fname);
-    })
 
     // Retrieve all Students
     router.get("/", register.getAllStudents);
--- Frontend_React/src/components/Register.js
+++ Frontend_React/src/components/Register.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import Axios from 'axios';
 import React, {useState} from 'react';
 
 import {
@@ -7,228 +7,90 @@ import {
 } from './common'
 import './Register.css';
 
-function Register() {
+function Markers() {
+  const [name, setName] = useState();
+  const [lname, setLName] = useState();
+  const [email, setEmail] = useState();
+  const [fPref, setFirstPref] = useState();
+  const [hours, setHours] = useState();
+  const [description, setDescription] = useState();
+  const [file, setFile] = useState();
 
-    /*Dynamic form field variables*/
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    /*
-    const [password, setPassword] = useState("");
-    const [password2, setPassword2] = useState("");
-    */
-    const [firstPref, setFirstPref] = useState(null);
-    const [secondPref, setSecondPref] = useState(null);
-    const [thirdPref, setThirdPref] = useState(null);
-    const [location, setLocation] = useState("");
-    const [hours, setHours] = useState(0);
-    const [file, setFile] = useState("");
-    const [filename, setFilename] = useState("Upload CV...");
+  const send = event => {
+    const data = new FormData();
+    data.append("firstName", name);
+    data.append("lastName",lname)
+    data.append("email",email)
+    data.append("fPref",fPref)
+    data.append("hours",hours)
+    data.append("description",description)
 
+    data.append("file", file);
 
-    /*real-time updating based on form fields input*/
-    const onChangeFirstName = (event) => {
-        setFirstName(event.target.value);
-    };
-    const onChangeLastName = (event) => {
-        setLastName(event.target.value);
-    };
-    const onChangeEmail = (event) => {
-        setEmail(event.target.value);
-    };
-{/*
-    const onChangePassword = (event) => {
-        setPassword(event.target.value);
-    };
-    const onChangePassword2 = (event) => {
-        setPassword2(event.target.value);
-    };
-*/}
-    const onChangeFirstPref = (event) => {
-        setFirstPref(event.target.value);
-    };
-    const onChangeSecondPref = (event) => {
-        setSecondPref(event.target.value);
-    };
-    const onChangeThirdPref = (event) => {
-        setThirdPref(event.target.value);
-    }
-    const onChangeHours = (event) => {
-        setHours(event.target.value);
-    }
-    const onChangeLocation = (event) => {
-        setLocation(event.target.value);
-    }
-    const onChangeFile = (event) => {
-        setFile(event.target.files[0]);
-        setFilename(event.target.files[0].name);
-    };
+    Axios.post("/api/uploadregistrationform", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
 
-    /*Upload to server using post request*/
-    const uploadUser = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form action="#">
+          <div className="flex">
+            <label htmlFor="name">First Name</label>
+            <input type="text" id="name" onChange={event => {
+                const { value } = event.target;
+                setName(value);
+              }}
+            />
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lname" onChange={event => {
+                const { value } = event.target;
+                setLName(value);
+              }}
+            />
+            <label htmlFor="email">Email Address</label>
+            <input type="email" id="email" onChange={event => {
+                const { value } = event.target;
+                setEmail(value);
+              }}
+            />
+            <label htmlFor="fPref">Course Preference</label>
+            <input type="text" id="fPref" onChange={event => {
+                const { value } = event.target;
+                setFirstPref(value);
+              }}
+            />
+            <label htmlFor="hours">Hours</label>
+            <input type="text" id="hours" onChange={event => {
+                const { value } = event.target;
+                setHours(value);
+              }}
+            />
+            <label htmlFor="Description">What is the nature of your physical availablity to mark on UoA Campus</label>
+            <input type="text" id="description" onChange={event => {
+                const { value } = event.target;
+                setDescription(value);
+              }}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="file">CV Upload</label>
+            <input
+              type="file"
+              id="file"
+              accept=".jpg"
+              onChange={event => {
+                const file = event.target.files[0];
+                setFile(file);
+              }}
+            />
+          </div>
+        </form>
+        <button onClick={send}>Send</button>
+      </header>
+    </div>
+  );
+}
 
-        var formData = new FormData();
-       // formData.append('file', file);    //add file after appending data
-        
-        var data = {
-             firstName: firstName,
-            lastName: lastName, 
-            firstPref: firstPref,
-            secondPref: secondPref,
-            thirdPref: thirdPref,
-            email: email,
-            pdfLocation: filename
-          };
-        formData.append('data', data);
-        formData.append('file', file);
-        
-        try {  
-        axios.post('/api/submit', [data, formData], {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-        .then(response => {console.log(response)});
-        } catch (err) {
-        if (err.response.status === 500) {
-          console.log('There was a problem with the server');
-        } else {
-          console.log(err)
-        }
-      }
-    
-    };
-
-    const load = () => {
-        const student1 = {
-            firstName:"Steven", 
-            lastName:"Kan", 
-            email:"Steven@gmail.com", 
-            password: null, 
-            firstPref: 1,
-            secondPref: null,
-            thirdPref: null,
-            hours: 10.7,
-            description: null, 
-            pdfLocation:null 
-        };
-        const student2 = {
-            firstName:"Andrew", 
-            lastName:"Pakwing", 
-            email:"Andrew@gmail.com", 
-            password: null, 
-            firstPref: 5,
-            secondPref: 6,
-            thirdPref: 2,
-            hours: 8,
-            description: null, 
-            pdfLocation:null 
-        };
-        const student3 = {
-            firstName:"Dave", 
-            lastName:"Wright", 
-            email:"Dave@gmail.com", 
-            password: null, 
-            firstPref: 3,
-            secondPref: 4,
-            thirdPref: null,
-            hours: 20,
-            description: null, 
-            pdfLocation:null 
-        };
-        const student4 = {
-            firstName:"Spencer", 
-            lastName:"Smith", 
-            email:"Spencer@gmail.com", 
-            password: null, 
-            firstPref: 6,
-            secondPref: 4,
-            thirdPref: 2,
-            hours: 15,
-            description: null, 
-            pdfLocation:null 
-        };
-        const student5 = {
-            firstName:"Sukrat", 
-            lastName:"Ubha", 
-            email:"Sukrat@gmail.com", 
-            password: null, 
-            firstPref: 1,
-            secondPref: 3,
-            thirdPref: null,
-            hours: 18.7,
-            description: null, 
-            pdfLocation:null 
-        };
-        axios.post("/api/students/api/registerStudent", student1);
-        axios.post("/api/students/api/registerStudent", student2);
-        axios.post("/api/students/api/registerStudent", student3);
-        axios.post("/api/students/api/registerStudent", student4);
-        axios.post("/api/students/api/registerStudent", student5)
-    };
-    
-
-
-    /*HTML component rendered on client page*/
-    return(
-        
-        <BoxComponent>
-            <button onClick={load}>Load Student</button>
-            <form onSubmit={uploadUser}>
-                <h1>Marker application form</h1>
-                <div className="formField">
-                    <label htmlFor='firstName' className='formLabel'> First name </label>
-                    <input id="firstName" type='text' name='firstName' className='formInput' placeholder='First name'
-                    value={firstName} onChange={onChangeFirstName}/>
-                </div>
-                <div className="formField">
-                    <label htmlFor='lastName' className='formLabel'> Last name </label>
-                    <input id="lastName" type='text' name='lastName' className='formInput' placeholder='Last name'
-                    value={lastName} onChange={onChangeLastName}/>
-                </div>
-                <div className="formField">
-                    <label htmlFor='email' className='formLabel'> Email </label>
-                    <input type='email' name='email' className='formInput' placeholder='abc123@gmail.com'
-                    value={email} onChange={onChangeEmail}/>
-                </div>
-                {/*
-                <div className="formField">
-                    <label htmlFor='password' className='formLabel'> Password </label>
-                    <input type='password' name='password' className='formInput' placeholder='password'
-                    value={password} onChange={onChangePassword}/>
-                </div>
-                <div className="formField">
-                    <label htmlFor='password2' className='formLabel'> Confirm password </label>
-                    <input type='password2' name='password2' className='formInput' placeholder='confirm password'
-                    value={password2} onChange={onChangePassword2}/>
-                </div>
-                */}
-                <div className="formField">
-                    <label htmlFor='preferences' className='formLabel'> Course preferences </label> 
-                    <div name='preferences'>
-                        <input type="text" className="searchbar" placeholder="search courses..."
-                        value={firstPref} onChange={onChangeFirstPref}/>
-                    </div>
-                </div>
-                <div className="formField">
-                    <label htmlFor='hours' className='formLabel'> Hours </label>
-                    <input type="text" className="formLabel" placeholder="0"
-                    value={hours} onChange={onChangeHours}/>
-                </div>
-                <div className="formField">
-                    <label htmlFor='location' className='formLabel'> What is the nature of your physical availablity to mark on UoA Campus? </label>
-                    <input type="text" className="formInput" style={{width: '75%', height: '100px'}}
-                    placeholder="describe circumstances here..."/>
-                </div>
-                <div className="formField">
-                    <label htmlFor='studentCV' className='formLabel'> CV Upload: </label>
-                    <input type='file' name='studentCV' className='formInput'
-                    value={file} onChange={onChangeFile}/>   
-                </div>   
-                <RegisterButton type="submit"> Register </RegisterButton>   
-            </form>
-        </BoxComponent>
-    )
-} 
-
-export default Register;
+export default Markers;
