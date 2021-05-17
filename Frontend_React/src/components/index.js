d72c265f9393968c96c4337d6f847246b8d41a5c
Steve-XD <qkan100@aucklanduni.ac.nz>
2021-05-17 23:25:54 +1200

    not showing right panel if nothing has been selected

--- Frontend_React/src/components/index.js
+++ Frontend_React/src/components/index.js
@@ -10,7 +10,7 @@ export default class index extends Component{
         super(props);
         this.state={
             course:"",
-            assoc:""
+            assoc:null
         }
     }
     parentFunction=(retrieved_course, retrieved_assoc)=>{
@@ -21,22 +21,27 @@ export default class index extends Component{
     }
 
     render() {
-            return (
-                <ReflexContainer orientation="vertical">
-                        <ReflexElement className="left-pane">
-                            <div className="pane-content" style={{overflow:"scroll"}}>
-                                <Main functionCallFromParent={this.parentFunction}/>
-                            </div>
-                        </ReflexElement>
-
-                        <ReflexSplitter style={{"zIndex":'0'}}/>
-
-                        <ReflexElement className="right-pane" size="450">
-                            <div className="pane-content" style={{background:'white', overflow:"scroll"}}>
-                                <Course_right dataFromParent={this.state.course} dataFromAssoc={this.state.assoc}/>
-                            </div>
+        let condition
+        
+        if (this.state.assoc!==null){ 
+            condition=<Course_right dataFromParent={this.state.course} dataFromAssoc={this.state.assoc}/>
+        }
+        return (
+            <ReflexContainer orientation="vertical">
+                    <ReflexElement className="left-pane">
+                        <div className="pane-content" style={{overflow:"scroll"}}>
+                            <Main functionCallFromParent={this.parentFunction}/>
+                        </div>
                     </ReflexElement>
-                </ReflexContainer>
-            );
+
+                    <ReflexSplitter style={{"zIndex":'0'}}/>
+
+                    <ReflexElement className="right-pane" size="450">
+                        <div className="pane-content" style={{background:'white', overflow:"scroll"}}>
+                            {condition}
+                        </div>
+                </ReflexElement>
+            </ReflexContainer>
+        );
         }
     }
