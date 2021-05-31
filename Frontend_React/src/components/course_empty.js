62ef62a0b7dc6e3f27cc5bac955ef6d9b799434e
Steve-XD <qkan100@aucklanduni.ac.nz>
2021-06-01 07:01:34 +1200

    Update main page design logo

--- Frontend_React/src/components/course_empty.js
+++ Frontend_React/src/components/course_empty.js
@@ -7,7 +7,11 @@ export default class course_empty extends Component{
                 <div>
                     <img src={logo} style={{display:"block", "marginLeft": "auto", "paddingTop": "200px",
                             "marginRight": "auto", width:"400px" }} alt="marcLogo"/>
+                    <br/>
+                    <h3 style={{textAlign:"center"}}>Marker Allocation Reconciliation Configurator</h3>
+                    
                 </div>
+                
             )}
     
 }
